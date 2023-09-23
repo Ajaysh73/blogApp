6e5d28ad9fdc01818939cceb32a7e47923e7a1f8
Ajay Sharma <ajaysh73@gmail.com>
2023-09-23 13:21:59 -0400

    master-basic structure of the application in place

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './categoryList.module.css';
+import Pagination from '../pagination/Pagination';
+const CategoryList = () => {
+  return (
+    <div className={styles.container}>
+      <Pagination />
+    </div>
+  );
+};
+
+export default CategoryList;
