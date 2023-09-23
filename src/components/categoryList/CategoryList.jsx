529a6396f6d49f90f8717c703193b72c7dcdfe10
Ajay Sharma <ajaysh73@gmail.com>
2023-09-23 14:28:33 -0400

    master-work on navbar

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import styles from './categoryList.module.css';
 import Pagination from '../pagination/Pagination';
 const CategoryList = () => {
-  return (
-    <div className={styles.container}>
-      <Pagination />
-    </div>
-  );
+  return <div className={styles.container}>CategoryList</div>;
 };
 
 export default CategoryList;
