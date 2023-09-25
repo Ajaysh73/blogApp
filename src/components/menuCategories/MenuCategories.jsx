405f2a59bf4599804dca1a73900eb20657d47662
Ajay Sharma <ajaysh73@gmail.com>
2023-09-25 15:02:54 -0400

    dev-menu and categories component added to home page

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import styles from './cardList.module.css';
-const CardList = () => {
-  return <div className={styles.container}> CardList</div>;
+import styles from './menuCategories.module.css';
+import Link from 'next/link';
+const MenuCategories = () => {
+  return (
+    <div className={styles.categoryList}>
+      <Link href='/blog?cat=style' className={`${styles.categoryItem} ${styles.style}`}>
+        Style
+      </Link>
+      <Link href='/blog' className={`${styles.categoryItem} ${styles.fashion}`}>
+        Fashion
+      </Link>
+      <Link href='/blog' className={`${styles.categoryItem} ${styles.food}`}>
+        Food
+      </Link>
+      <Link href='/blog' className={`${styles.categoryItem} ${styles.travel}`}>
+        Travel
+      </Link>
+      <Link href='/blog' className={`${styles.categoryItem} ${styles.culture}`}>
+        Culture
+      </Link>
+      <Link href='/blog' className={`${styles.categoryItem} ${styles.coding}`}>
+        Coding
+      </Link>
+    </div>
+  );
 };
 
-export default CardList;
+export default MenuCategories;
